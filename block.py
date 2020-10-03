9129eae82c2df9a2b707d43125f0a17949f3fb48
Joe Raccio <joeraccioiv@gmail.com>
2020-10-03 16:20:29 -0400

    Max tag intergration

--- block.py
+++ block.py
@@ -1,12 +1,13 @@
 #scheudle broken up in blocks, each block is associated with a weight
 
 class block:
-  def __init__(self, start, end, weight = 0): 
+  def __init__(self, start, end, weight = 0, tag=set()): 
     if start > end: #takes care of overlapping, makes it easier to compare
       end += 24
     self.start = start;
     self.end = end;
     self.weight = weight;
+    self.tags = tag; #tags is a dictionary used to limit the ammount of events in one day
 
   def __repr__(self): #printing out string, (start, end)
     return "(" + str(self.start) +","+ str(self.end)+")"
@@ -16,3 +17,4 @@ def overlapping(block1, block2): #returns true if blocks overlap
     return True; 
   else:
     return False;
+
