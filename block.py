455d48ed8f554d4f4a56d4af093ec80b7bd01b59
Kevin Freyberg <56777568+kevinfreyberg@users.noreply.github.com>
2020-10-03 14:37:54 -0400

    Update block.py

--- block.py
+++ block.py
@@ -1,16 +1,18 @@
+#scheudle broken up in blocks, each block is associated with a weight
+
 class block:
-  def __init__(self, start, end, weight = 0):
-    if start > end:
+  def __init__(self, start, end, weight = 0): 
+    if start > end: #takes care of overlapping, makes it easier to compare
       end += 24
     self.start = start;
     self.end = end;
     self.weight = weight;
 
-  def __repr__(self):
+  def __repr__(self): #printing out string, (start, end)
     return "(" + str(self.start) +","+ str(self.end)+")"
 
-def overlapping(block1, block2):
+def overlapping(block1, block2): #returns true if blocks overlap
   if (block2.start<block1.start<block2.end) or (block2.start<block1.end<block2.end):
-    return True;
+    return True; 
   else:
     return False;
