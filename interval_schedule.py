f94b8316be6a11a45b8f798e886d1f4ad1d3c8b0
Joe Raccio <joeraccioiv@gmail.com>
2020-10-04 15:00:30 -0400

    Multiple people/shifts implemented

--- interval_schedule.py
+++ interval_schedule.py
@@ -1,7 +1,8 @@
 import block as b
 import schedule as s
 
-def schedule(lst):
+
+def optimize_schedule(lst):
   #List is a list of blocks
   
   #Sort list by rising end time
@@ -10,7 +11,7 @@ def schedule(lst):
   
   #Solution array keeps track of what blocks are use
   #This allows us to return the solution as a list of blocks
-  solution_array = [] 
+  solution_array = []
   max_weight_index = 0
 
   # If you want to limit the ammount a tag can show up ex
@@ -33,43 +34,27 @@ def schedule(lst):
   # i's original value + j's weight_array_value
   for i in range(0,len(lst)-1):
     for j in range(1,i): 
-      
       #A tag conflit is when you have to many of one type of event. Ex: 2 Sleep times
       if not s.tag_conflict(solution_array[i], lst[j], max_tags):
         continue
       
       #If there is a time overlap, there is nothing to do
       elif (b.overlapping(lst[j], lst[i])):
+
         continue
       
       else:
         #If i's orginal value + j's weight array > i's current weight_array
+        #You also need to clear and reset the current solution if the one you
+        #just found is better
         if lst[i].weight + weight_array[j] > weight_array[i]:
           weight_array[i] = lst[i].weight + weight_array[j]
-          solution_array[i].insert_block(lst[j])
+          solution_array[i].clear()
+          solution_array[i].copy(solution_array[j])
+          solution_array[i].insert_block(lst[i])
           #If weight array was updated then check to see if its the new max
           if weight_array[i] > weight_array[max_weight_index]:
             max_weight_index = i
 
   solution_array[max_weight_index].lst.sort(key=lambda x: x.start)
-  print(weight_array[max_weight_index])
   return solution_array[max_weight_index]
-
-#List of tags
-food_tag = set(["Food"])
-sleep_tag = set(["Sleep"])
-
-
-# Testing
-lst = []
-lst.append(b.block(1,3,5))
-lst.append(b.block(2,5,6))
-lst.append(b.block(4,6,5))
-lst.append(b.block(6,7,4))
-lst.append(b.block(7,9,2))
-lst.append(b.block(5,8,11))
-
-duration = 8;
-for start in range(0,24-duration, 1):
-  lst.append(b.block(start, start+duration, 15, sleep_tag))
-
