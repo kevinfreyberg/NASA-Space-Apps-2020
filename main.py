44ddde00776b991540e1a817cd92e636d052dde7
Kevin Freyberg <kfreyberg01@gmail.com>
2020-10-04 21:26:00 -0400

    Added new tags, generation of schedule

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import block as b
 import interval_schedule
 
 
-#List of tags
-food_tag = set(["Food"])
-sleep_tag = set(["Sleep"])
-max_tags = {"Food":3, "Sleep":1}
+#List of tags, weights 1->4 depending on importance
+exercise_tag = set(["Exercise"]) #weight 1
+work_tag = set(["Work"]) #weight 1
+food_tag = set(["Food"]) #weight 2
+sleep_tag = set(["Sleep"]) #weight 3
+dock_tag = set(["Docking"]) #weight 4
+launch_tag = set("Launching") #weight 4
+landing_tag = set(["Landing"]) #weight 4
+undock_tag = set(["Undocking"]) #weight 4
 
+#Food = 1 hour per, Sleep is defined by input, Exercise = 1 hour, Work = 1 hour
+max_tags = {"Food":3, "Sleep":1, "Exercise":1, "Work":12} 
 
 shifts = []
 people = []
@@ -20,28 +27,43 @@ sleep_value_eq = lambda x: 100-(person.sleep_offset(x)**2)
 
 #shift1 is the first job that needs to be filled
 #you add all of the tasks shift one could possibly do to a list
-shift1 = []
-shift1.append(b.block(1,3,5))
-shift1.append(b.block(2,5,6))
-shift1.append(b.block(4,6,5))
-shift1.append(b.block(6,7,4))
-shift1.append(b.block(7,9,2))
-shift1.append(b.block(5,8,11))
-shifts.append(shift1)
 
-shift2 = []
-shift2.append(b.block(20,24,6))
-shift2.append(b.block(16,19,7))
-shift2.append(b.block(15,16,3))
-shift2.append(b.block(12,15,5))
-shift2.append(b.block(14,17,6))
-shift2.append(b.block(13,16,8))
-shifts.append(shift2)
+shift1 = [] #Hours 8-24
+for time in range(0, 24): #generates time blocks for all activities
+  shift1.append(b.block(time,time+1,10,exercise_tag))
+  shift1.append(b.block(time,time+1,2,work_tag))
+  shift1.append(b.block(time,time+1,10,food_tag))
+
+
 
 #All astronauts and their sleepschedules go here
-people.append(p.person("a", 0,8))
-people.append(p.person("b",16,24))
+#people.append(p.person("a", 0,8))
+#people.append(p.person("b",16,24))
 
+print("Welcome to our sleep shift scheduling tool! What is your name?\n")
+name = input()
+print("\nHi, {}! When do you normally go to sleep (24H Format, round by hour)?\n".format(name))
+sleep_start = int(input())
+print("\nWhen do you normally wake up (24H Format, round by hour)?\n")
+sleep_end = int(input())
+print("\nWould you consider yourself a morning-person (a), night-person (b), or in-between (c)?\n")
+choice = str(input())
+
+possible_choices = ["a", "b", "c"]
+while (choice not in possible_choices):
+  print("\nInvalid input!\n")
+  print("\nWould you consider yourself a morning-person (a), night-person (b), or in-between (c)?\n")
+  choice = str(input())
+
+people.append(p.person(name, sleep_start, sleep_end))
+
+#adds shifts based on preferences
+if choice == "a":
+  shifts.append(shift1)
+elif choice == "b":
+  shifts.append(shift1)
+else:
+  shifts.append(shift1)
 
 #See who has the best outcome for shift 1: place them in shift 1
 #See who has the best outcome for shift 2: place them in shift 2
@@ -65,4 +87,8 @@ for pair in pairs:
   print("Person:", pair[1])
   print("Schedule:", pair[0])
   print()
+
+
+
+
 
